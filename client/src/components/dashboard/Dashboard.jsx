6c3560682c8115abce399f3adc027c6051ba37fc
OmerShuvami <omershuva3@gmail.com>
2024-03-05 12:24:31 +0200

    added guest login

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,23 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
 
-const navigate = useNavigate()
+  const navigate = useNavigate();
 
-const handleExistingTrip = () =>{
-    navigate("trip-planner")
-}
-const handleNewTrip = () =>{
-    navigate("new-trip")
-    
-}
+  const handleExistingTrip = () => {
+    navigate("trip-planner");
+  };
+  const handleNewTrip = () => {
+    navigate("new-trip");
+  };
 
   return (
     <div>
       <button onClick={handleNewTrip}>New Trip</button>
       <button onClick={handleExistingTrip}>Trip</button>
-      
     </div>
   );
 }
---
+++ client/src/utils/AuthService.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+
+export const signup = async (userData) => {
+  return axios.post(`${import.meta.env.VITE_API_URL}/users/register`, userData);
+};
+
+export const login = async (credentials) => {
+  return axios.post(`${import.meta.env.VITE_API_URL}/users/login`, credentials);
+};
+export const logout = async () => {
+  try {
+    await axios.get(`${import.meta.env.VITE_API_URL}/users/logout`, {
+      withCredentials: true,
+    });
+  } catch (error) {
+    console.error("Error logging out:", error);
+  }
+};
+
+// export const authenticate = async () => {
+//   return await axios.get(`${import.meta.env.VITE_API_URL}/users/authenticate`, {
+//     withCredentials: true,
+//   });
+// };
