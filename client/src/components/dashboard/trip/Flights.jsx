19c99691e1701a0994fbc0873318e9b397cbb104
omer nofarber <dr.fingerles@gmail.com>
2024-03-08 11:08:57 +0200

    flight component

--- client/src/components/dashboard/trip/Flights.jsx
+++ client/src/components/dashboard/trip/Flights.jsx
@@ -1,34 +1,110 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { AppContext } from "../../../context/AppContext";
 import { checkForUser } from "../../../utils/AuthService";
+import airportData from '../../../assets/airport.json'
+import flags from '../../../assets/flags.json'
+import { findFlights } from "../../../utils/CRUDService";
 
 
 function Flights() {
-  const {setUser} = useContext(AppContext)
-    const navigate = useNavigate();
+  const { setUser } = useContext(AppContext)
+  const navigate = useNavigate();
+  const [showFrom, setShowFrom] = useState([])
+  const [flightOrderObj, setFlightOrderObj] = useState({})
+  const [flightFound, setFlightFound] = useState([])
 
-    const handleGoBack = () => {
-      navigate(-1);
-    };
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+  const getflights = async (v) => {
+    const flights = await findFlights(v)
+    console.log(flights.data);
+    setFlightFound(flights.data)
+  };
 
-    useEffect(() => {
-      checkForUser().then((response) => {
-        if (response.data) {
-          setUser(response.data);
-        } else {
-          logout();
-          navigate("/");
-          alert("Your previous session has ended, please login again.");
-        }
-      });
-    }, []);
-    return (
-      <div>
-        <IoMdArrowRoundBack onClick={handleGoBack} className="go-back" /> Flights
-      </div>
+
+  const handleInputFrom = (e) => {
+    const lowerSearchString = e.toLowerCase();
+    const tempArr = airportData.filter(obj =>
+      Object.values(obj).some(value =>
+        typeof value === 'string' && value.toLowerCase().includes(lowerSearchString)
+      )
     );
+    console.log(tempArr.slice(0, 7));
+    setShowFrom(tempArr.slice(0, 7))
+    setFlightOrderObj({...flightOrderObj,depAP:e})
+  };
+  const handleInputTo = (e) => {
+    const lowerSearchString = e.toLowerCase();
+    const tempArr = airportData.filter(obj =>
+      Object.values(obj).some(value =>
+        typeof value === 'string' && value.toLowerCase().includes(lowerSearchString)
+      )
+    );
+    console.log(tempArr.slice(0, 7));
+    setShowFrom(tempArr.slice(0, 7))
+    setFlightOrderObj({...flightOrderObj,arrAP:e})
+  };
+
+  useEffect(() => {
+    checkForUser().then((response) => {
+      if (response.data) {
+        setUser(response.data);
+      } else {
+        logout();
+        navigate("/");
+        alert("Your previous session has ended, please login again.");
+      }
+    });
+  }, []);
+  return (
+    <div>
+      <IoMdArrowRoundBack onClick={handleGoBack} className="go-back" />
+      <div>
+        <span>from</span>
+        <input type="text" name="from" list="cityname" onChange={(e) => handleInputFrom(e.target.value)} />
+        <datalist id="cityname">
+          {
+            showFrom.map((v, i) =><div>
+              <option key={i} value={v.iata_code}> {v.name} | {v.country}</option>
+            </div>
+            )}
+        </datalist>
+        <span>          to</span>
+        <input type="text" name="to" list="cityname" onChange={(e) => handleInputTo(e.target.value)} />
+        <datalist id="cityname">
+          {
+            showFrom.map((v, i) =>
+              <option key={i} value={v.iata_code}>{v.name} | {v.country}</option>
+
+            )}
+        </datalist>
+        <br />
+        <span>at</span>
+        <input type="date" id="dateInput" min="13/09/2024" max="27/09/2024" onChange={(e)=>setFlightOrderObj({...flightOrderObj,depTM:e.target.value})}/>
+      <button onClick={()=>console.log(flightOrderObj)}>tst</button>
+      <button onClick={()=>getflights(flightOrderObj)}>order</button>
+      <div>
+        {flightFound.map((e,i)=>
+        <div style={{backgroundColor:"aliceblue"}} key={i}>
+          {e.flights.map((v,i1)=>
+          <div key={i1}>
+            <span>from {v.departure_airport.name} at {v.departure_airport.time}</span>
+            <br />
+            <span>from {v.arrival_airport.name} at {v.arrival_airport.time}</span>
+          </div>
+          )}
+          <span>price: {e.price}$ </span>
+        </div>
+        )}
+      </div>
+
+      </div>
+
+    </div>
+  );
 }
 
 export default Flights
--- server/controllers/getHotelController.js
+++ server/controllers/getHotelController.js
@@ -1,20 +1,24 @@
 const axios = require("axios")
+const { getJson } = require("serpapi");
 
 exports.getHotelInfo = async (req, res) => {
     try {
         console.error("aaa");
-        const response = await axios.get('https://test.api.amadeus.com/v1/shopping/flight-destinations', {
-            params: {
-                'origin': 'PAR',
-                'destination':'MAD',
-                'maxPrice': '200'
-            },
-            headers: {
-                'Authorization': 'Bearer GZNKxPcesN1jFKDK8ifNdYlsowXc'
-            }
+        const {arrAP,depAP,depTM} = req.body
+        await getJson({
+            engine: "google_flights",
+            hl: "en",
+            gl: "us",
+            type:2,
+            departure_id: depAP,
+            arrival_id: arrAP,
+            outbound_date: depTM,
+            currency: "USD",
+            api_key: "b6d282278cda6c559e0c1222985bcd1cf1ab749541ad43c3537a7bd8e5674406"
+        }, (json) => {
+            console.log(json.best_flights);
+            res.send(json.best_flights)
         });
-        console.log(response.data);
-        res.send(response.data)
     } catch (err) {
         console.log(err);
         res.status(404).json({
@@ -23,4 +27,3 @@ exports.getHotelInfo = async (req, res) => {
         });
     }
 };
-
